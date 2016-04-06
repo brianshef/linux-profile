8af43f2c9592dfa95d1465b5b565b31b9558827c
Brian Shef <brian.shef@rewardstyle.com>
2016-04-06 14:55:31 -0500

    Correct SSH key usage

--- scripts/devops.sh
+++ scripts/devops.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Automatically logs in to DevOps remote server
 SERVER="devops.va"
-SSH_KEY="~/.ssh/$SSH_KEY"
+SSH_KEY="$HOME/.ssh/brianshef"
 
-echo "Adding SSH key to session ... "
-ssh-add ~/.ssh/$SSH_KEY
+echo "Adding SSH key $SSH_KEY to session ... "
+ssh-add $SSH_KEY
 echo " ... done. Logging in now ... "
 ssh $SERVER
