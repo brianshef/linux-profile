63a4800f9bd9d6e6a3d4e5ade9566882064bb403
Brian Shef <brian.shef@rewardstyle.com>
2016-04-27 09:28:09 -0500

    Validate keys exist and added to session before attempting ssh connection

--- scripts/devops.sh
+++ scripts/devops.sh
@@ -3,10 +3,21 @@
 SERVER="devops.va"
 KEY_DIR="$HOME/.ssh"
 SSH_KEYS=( "$KEY_DIR/brianshef" "$KEY_DIR/id_rsa" )
-
+KEYS_ADDED=0
 for key in "${SSH_KEYS[@]}"; do
-  echo "Adding SSH key $SSH_KEY to session ... "
-  ssh-add $key
+  if [ -e "${key}" ]; then
+    echo "Adding SSH key $key to session ... "
+    ssh-add $key
+    (( KEYS_ADDED++ ))
+  else
+    echo "WARNING! $key not found! Skipping ... "
+  fi
 done
-echo " ... done. Logging in now (with agent forwarding enabled)... "
-ssh -A $SERVER
+if [ $KEYS_ADDED -gt 0 ]; then
+  echo " ... done adding $KEYS_ADDED key(s) to session."
+  echo "Logging in now (with agent forwarding enabled) ... "
+  ssh -A $SERVER
+else
+  echo " ... no key(s) added to session. Unable to SSH to $SERVER."
+  exit 0
+fi
